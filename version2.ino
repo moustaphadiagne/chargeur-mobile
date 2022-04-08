45e6cf5f3784c1bf013a0028772d0973d0bef4d2
moustaphadiagne <86981375+moustaphadiagne@users.noreply.github.com>
2022-04-08 11:12:05 +0000

    version 2 smartcharge
    petite modification dans la bibliotheque.

---
+++ version2.ino
@@ -0,0 +1,237 @@
+#include <Wire.h>
+#include "WiFi.h"
+#include "ESPAsyncWebServer.h"
+#include <Adafruit_INA260.h>
+
+const char* PARAM_INPUT_1 = "state";
+const int output = 14;
+int ledState = LOW;
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50; 
+
+#define TCAADDR 0x70
+// Replace with your network credentials
+const char* ssid = "ESPTTGO";
+const char* password = "ubuntu0000";
+void tcaselect(uint8_t i) {
+  if (i > 7) return;
+ 
+  Wire.beginTransmission(TCAADDR);
+  Wire.write(1 << i);
+  Wire.endTransmission();  
+}
+Adafruit_INA260 ina260 = Adafruit_INA260();
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+<title>CHARGEUR</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+    html {
+     font-family: Arial;
+     display: inline-block;
+     margin: 0px auto;
+     text-align: center;
+    }
+    h2 { font-size: 2.0rem; }
+    p { font-size: 2.0rem; }
+    .units { font-size: 1.2rem; }
+    .dht-labels{
+      font-size: 1.5rem;
+      vertical-align:middle;
+      padding-bottom: 15px;
+    }
+    .button { 
+    background-color: #4CAF50;
+    border: none;
+    color: white; 
+    font-size: 1.5rem;
+    vertical-align:middle;
+    }
+    .switch {position: relative; display: inline-block; width: 60px; height: 38px} 
+    .switch input {display: none}
+    .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #ccc; border-radius: 34px}
+    .slider:before {position: absolute; content: ""; height: 22px; width: 22px; left: 8px; bottom: 8px; background-color: #fff; -webkit-transition: .4s; transition: .4s; border-radius: 68px}
+    input:checked+.slider {background-color: #2196F3}
+    input:checked+.slider:before {-webkit-transform: translateX(24px); -ms-transform: translateX(24px); transform: translateX(24px)}
+  </style>
+</head>
+<body>
+  <h2>CHARGEUR MOBILE</h2>
+  %BUTTONPLACEHOLDER%
+  <script>function toggleCheckbox(element) {
+  var xhr = new XMLHttpRequest();
+  if(element.checked){ xhr.open("GET", "/update?state=1", true); }
+  else { xhr.open("GET", "/update?state=0", true); }
+  xhr.send();
+}
+
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var inputChecked;
+      var outputStateM;
+      if( this.responseText == 1){ 
+        inputChecked = true;
+        outputStateM = "MARCHE";
+      }
+      else { 
+        inputChecked = false;
+        outputStateM = "ARRET";
+      }
+      document.getElementById("output").checked = inputChecked;
+      document.getElementById("outputState").innerHTML = outputStateM;
+    }
+  };
+  xhttp.open("GET", "/state", true);
+  xhttp.send();
+}, 1000 ) ;
+</script>
+  <p>
+    <span class="dht-labels">INTENSITE</span> 
+    <span id="courant">%TEMPERATURE%</span>
+    <sup class="units">mA</sup>
+  </p>
+  <p>
+    <span class="dht-labels">TENSION</span>
+    <span id="tension">%TENSION%</span>
+    <sup class="units">mV</sup>
+  </p>
+  <p>
+    <span class="dht-labels">PUISSANCE</span>
+    <span id="puissance">%PUISSANCE%</span>
+    <sup class="units">mW</sup>
+  </p>
+</body>
+<script>
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("courant").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/temperature", true);
+  xhttp.send();
+}, 5000 ) ;
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("tension").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/tension", true);
+  xhttp.send();
+}, 5000 ) ;
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("puissance").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/puissance", true);
+  xhttp.send();
+}, 5000 ) ;
+</script>
+</html>)rawliteral";
+
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "TEMPERATURE"){
+    return "--";
+  }
+  else if(var == "TENSION"){
+    return "--";
+  }
+  else if(var == "PUISSANCE"){
+    return "--";
+  }
+  else if (var =="BUTTONPLACEHOLDER"){
+    String buttons ="";
+    String outputStateValue = outputState();
+    buttons+= "<h4>ETAT <span id=\"outputState\"></span></h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"output\" " + outputStateValue + "><span class=\"slider\"></span></label>";
+    return buttons;
+  }
+  return String();
+}
+String outputState(){
+  if(digitalRead(output)){
+    return "checked";
+  }
+  else {
+    return "";
+  }
+  return "";
+}
+
+void setup(){
+  pinMode(output, OUTPUT);
+  digitalWrite(output, LOW);
+  // Serial port for debugging purposes
+  Wire.begin();
+  Serial.begin(115200);
+  tcaselect(1);
+  if (!ina260.begin()) {
+    Serial.println("Couldn't find INA260 chip");
+    while (1);
+  }
+  // Connect to Wi-Fi
+ Serial.print("Mie en place hotspot…");
+  WiFi.softAP(ssid, password);  
+   
+  Serial.println( "" );  
+  Serial.println( "Demarrage" );  
+  Serial.println( "IP address: " );  
+  Serial.println( WiFi.softAPIP() );
+
+tcaselect(1);
+  //Serial.print("Current1: ");
+  //Serial.print(ina260.readCurrent());
+  //Serial.println(" mA");
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", String(ina260.readCurrent()).c_str());
+  });
+  server.on("/tension", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", String(ina260.readBusVoltage()).c_str());
+  });
+  server.on("/puissance", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", String(ina260.readPower()).c_str());
+  });
+  
+server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String inputMessage;
+    String inputParam;
+    // GET input1 value on <ESP_IP>/update?state=<inputMessage>
+    if (request->hasParam(PARAM_INPUT_1)) {
+      inputMessage = request->getParam(PARAM_INPUT_1)->value();
+      inputParam = PARAM_INPUT_1;
+      digitalWrite(output, inputMessage.toInt());
+      ledState = !ledState;
+    }
+    else {
+      inputMessage = "No message sent";
+      inputParam = "none";
+    }
+    Serial.println(inputMessage);
+    request->send(200, "text/plain", "OK");
+  });
+// Send a GET request to <ESP_IP>/state
+  server.on("/state", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", String(digitalRead(output)).c_str());
+  });
+  // Start server
+  server.begin();
+}
+ 
+void loop(){
+}
